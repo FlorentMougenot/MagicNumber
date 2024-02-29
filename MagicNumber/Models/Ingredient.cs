a2523f4c776c66faa6883d774371f1276c652375
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-02-29 10:01:16 +0100

    Ajoutez des fichiers projet.

---
+++ MagicNumber/Models/Ingredient.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicNumber.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public int PizzaId { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ MagicNumber/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicNumber.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public List<Ingredient>? Ingredients { get; set; }
+        public string? ImageLink { get; set; }
+        public int QuantityInCart { get; set; }
+    }
+}
